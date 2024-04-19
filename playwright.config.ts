12f3e81bdc96b15c8e93e5d47980a7a12e022792
srutip04 <subuhishruti75@gmail.com>
2024-04-19 10:36:35 +0530

    adding storage state

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,42 +1,30 @@
-import { defineConfig, devices } from '@playwright/test';
+// playwright.config.ts
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+import { defineConfig, devices } from "@playwright/test";
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+export const STORAGE_STATE = "./auth/session.json";
+
 export default defineConfig({
-  testDir: './e2e',
-  /* Run tests in files in parallel */
+  testDir: "./e2e",
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: "html",
   use: {
+    trace: "on-first-retry",
     baseURL: "http://localhost:3000",
-    trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "login",
       use: { ...devices["Desktop Chrome"] },
-      testMatch: "**/login.spec.ts",
+      testMatch: "**/login.setup.ts", // Notice the updated spec name
     },
     {
       name: "Logged In tests",
-      use: { ...devices["Desktop Chrome"] },
+      use: { ...devices["Desktop Chrome"], storageState: STORAGE_STATE },
       dependencies: ["login"],
       testMatch: "**/*.spec.ts",
       testIgnore: "**/register.spec.ts",
@@ -47,11 +35,4 @@ export default defineConfig({
       testMatch: "**/register.spec.ts",
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
